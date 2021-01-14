5167001f7263236ef1b22a2095c3d965d2014c56
Guilherme Rocha <guilherme.rocha@usemobile.xyz>
2021-01-14 14:31:14 -0300

    feat: client added to ApiGithub.kt

--- app/src/main/java/br/com/guilherme/githubreposlist/data/ApiGithub.kt
+++ app/src/main/java/br/com/guilherme/githubreposlist/data/ApiGithub.kt
@@ -2,8 +2,11 @@ package br.com.guilherme.githubreposlist.data
 
 import br.com.guilherme.githubreposlist.BuildConfig
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ApiGithub {
 
@@ -12,11 +15,13 @@ class ApiGithub {
         private var retrofit: Retrofit? = null
         var apiGithub = connectionRetrofit()?.create(ApiGithubInterface::class.java)
 
+
         private fun connectionRetrofit(): Retrofit? {
             if (retrofit == null)
                 retrofit = Retrofit.Builder()
                     .baseUrl(BuildConfig.SERVER_URL)
                     .addConverterFactory(GsonConverterFactory.create(mGson))
+                    .client(mClient)
                     .build()
 
             return retrofit
@@ -25,6 +30,18 @@ class ApiGithub {
         private var mGson = GsonBuilder()
             .setLenient()
             .create()
+
+        private fun getLoggingInterceptor(): HttpLoggingInterceptor {
+            return HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BASIC)
+        }
+
+        private val mClient = OkHttpClient.Builder()
+            .addInterceptor(getLoggingInterceptor())
+            .connectTimeout(0, TimeUnit.SECONDS)
+            .readTimeout(0, TimeUnit.SECONDS)
+            .writeTimeout(0, TimeUnit.SECONDS)
+            .build()
 
     }
 }
