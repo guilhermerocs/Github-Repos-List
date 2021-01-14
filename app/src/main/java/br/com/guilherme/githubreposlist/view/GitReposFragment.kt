1e57f6b8e1c5f04a3fe2eea95e2b2e26d78550af
Guilherme Rocha <guilherme.rocha@usemobile.xyz>
2021-01-14 17:58:28 -0300

    feat: GitReposFragment.kt done

--- app/src/main/java/br/com/guilherme/githubreposlist/view/GitReposFragment.kt
+++ app/src/main/java/br/com/guilherme/githubreposlist/view/GitReposFragment.kt
@@ -7,25 +7,50 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.guilherme.githubreposlist.R
+import br.com.guilherme.githubreposlist.data.model.entity.GitRepository
+import br.com.guilherme.githubreposlist.databinding.FragmentGitReposBinding
 
 class GitReposFragment : Fragment(R.layout.fragment_git_repos) {
 
     lateinit var viewModelGit: GitRepositoriesViewModel
 
+    private var _binding: FragmentGitReposBinding? = null
+    private val binding get() = _binding!!
+
+    private var reposAdapter: GitReposAdapter? = null
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_git_repos, container, false)
+    ): View {
+        _binding = FragmentGitReposBinding.inflate(inflater, container, false)
+        return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpRecycler()
         initViewModel()
         observeViewModel()
 
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setUpRecycler() {
+        binding.reposRecycler.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            reposAdapter = GitReposAdapter(arrayListOf())
+            adapter = reposAdapter
+        }
     }
 
     private fun initViewModel() {
@@ -40,9 +65,13 @@ class GitReposFragment : Fragment(R.layout.fragment_git_repos) {
             }
 
             gitRepos.observe(viewLifecycleOwner) {
-
+                fillGitRepos(it)
             }
         }
+    }
+
+    private fun fillGitRepos(repos: List<GitRepository>) {
+        reposAdapter?.addAll(repos as ArrayList<GitRepository>)
     }
 
     private fun showError(message: String) {
