d238b0e0c86c5f5414b13dacdc6e37e98fa75bf4
Guilherme Rocha <guilherme.rocha@usemobile.xyz>
2021-03-10 09:47:57 -0300

    feat: useCase interface added do Module.kt DI

--- app/src/main/java/br/com/guilherme/githubreposlist/di/Module.kt
+++ app/src/main/java/br/com/guilherme/githubreposlist/di/Module.kt
@@ -3,11 +3,18 @@ package br.com.guilherme.githubreposlist.di
 import br.com.guilherme.githubreposlist.data.repository.GitReposRepositoryImp
 import br.com.guilherme.githubreposlist.data.source.remote.GitRepositoriesRemote
 import br.com.guilherme.githubreposlist.domain.repository.GitReposRepository
+import br.com.guilherme.githubreposlist.domain.usecase.FetchReposUseCase
+import br.com.guilherme.githubreposlist.domain.usecase.FetchReposUseCaseI
 import dagger.Module
 import dagger.Provides
 
 @Module
 class Module {
+
+    @Provides
+    fun providesFetchReposUseCase(): FetchReposUseCaseI {
+        return FetchReposUseCase(providesGitReposRepository())
+    }
 
     @Provides
     fun providesGitReposRepository(): GitReposRepository {
